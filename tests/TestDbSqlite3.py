ba5fd5ea37d7bbfff5bf440cee809021d3bded6a
Daniel Kettle <initial.dann@gmail.com>
2013-09-18 16:56:07 -0600

    Fixed sqlite3 tests

--- tests/TestDbSqlite3.py
+++ tests/TestDbSqlite3.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.join(os.getcwd(), '..'))
 sys.path.append(os.path.join(os.getcwd(), '..', 'adapters'))
 
 import unittest
-from squall import Table, Fields, Value, Where, Verbatim
+from squall import *
 import squallsql, sqlite3
 
 class Test(unittest.TestCase):
@@ -45,20 +45,20 @@ class Test(unittest.TestCase):
         assert not self.driver.sqladapter.cursor is None, 'Cursor not initialized'
         
     def testInsertDeleteUpdate(self):
-        print("Test: Sqlite3 Select, Insert, Delete and Update")
+        print("testInsertDeleteUpdate")
         trans = self.driver.Transaction()
-        self.sqlselect = self.driver.Select(Table('t'), Fields('*'), Where('x', '=', Value(1), []))
+        self.sqlselect = Select(Table('t'), Fields('*'), condition=Where('x', '=', Value(1)))
         assert not self.sqlselect is None, 'Select() object not initialized'
         trans.add(self.sqlselect)
-        self.sqlinsert = self.driver.Insert(Table('t'), Fields(), [Value(1), Value(2), Value(3)])
+        self.sqlinsert = Insert(Table('t'), Fields(), [Value(1), Value(2), Value(3)])
         assert not self.sqlinsert is None, 'Insert() object not initialized'
         trans.add(self.sqlinsert)
-        self.sqldelete = self.driver.Delete(Table('t'), Where('x', '=', Value(1)))
+        self.sqldelete = Delete(Table('t'), condition=Where('x', '=', Value(1)))
         assert not self.sqldelete is None, 'Delete() object not initialized'
         trans.add(self.sqldelete)
-        self.sqlupdate = self.driver.Update(Table('t'), Fields('y', 'z'), 
+        self.sqlupdate = Update(Table('t'), Fields('y', 'z'), 
                                               (Value(5), Value(9)), 
-                                               Where('x', '=', Value(1)))
+                                               condition=Where('x', '=', Value(1)))
         assert not self.sqlupdate is None, 'Update() object not initialized'
         trans.add(self.sqlupdate)
         trans.run()
@@ -70,27 +70,26 @@ class Test(unittest.TestCase):
         # TODO: Add non-committed insert, attempt select on it (expect fail), then insert and select (success)
           
     def testSqliteDelete(self):
-        print("Test: Sqlite3 Insert and Delete")
-        trans = self.driver.Transaction(self.driver.Insert(Table('t'), Fields(), [Value(1), Value(2), Value(3)]),
-                                        self.driver.Delete(Table('t'), Where('x', '=', Value(1))))
-        trans2 = self.driver.Transaction(self.driver.Insert(Table('t'), Fields(), [Value(1), Value(2), Value(3)]),
-                                        self.driver.Delete(Table('t'), Where('x', '=', Value(1))))
+        print("testSqliteDelete")
+        trans = self.driver.Transaction(Insert(Table('t'), Fields(), [Value(1), Value(2), Value(3)]),
+                                        Delete(Table('t'), condition=Where('x', '=', Value(1))))
+        trans2 = self.driver.Transaction(Insert(Table('t'), Fields(), [Value(1), Value(2), Value(3)]),
+                                         Delete(Table('t'), condition=Where('x', '=', Value(1))))
         trans.run()
-        print("Test: Sqlite3 force rollback on Insert/Delete transaction")
-        self.assertRaises(sqlite3.IntegrityError, trans2.run, force='rollback')
+        self.assertRaises(RollbackException, trans2.run, force='rollback')
           
     def testSqliteUpdate(self):
-        print("Test: Sqlite3 Insert and Update")
-        self.driver.Transaction(self.driver.Insert(Table('t'), Fields(), [Value(1), Value(2), Value(3)])).run()
-        self.driver.Transaction(self.driver.Update(Table('t'), Fields('y', 'z'), 
+        print("testSqliteUpdate")
+        self.driver.Transaction(Insert(Table('t'), Fields(), [Value(1), Value(2), Value(3)])).run()
+        self.driver.Transaction(Update(Table('t'), Fields('y', 'z'), 
                                               (Value(5), Value(9)), 
                                                Where('x', '=', Value(1)))).run()
          
     def testSelectReturn(self):
-        print("Testing return value of Select")
+        print("testSelectReturn")
         t = self.driver.Transaction(self.driver.Insert(
                 Table('t'), Fields(), [Value(1), Value(2), Value(3)]))
-        sqlselect = self.driver.Select(Table('t'), Fields('*'), Where('x', '=', Value(1)))
+        sqlselect = Select(Table('t'), Fields('*'), Where('x', '=', Value(1)))
         t.add(sqlselect)
         print(str(sqlselect))
         output = t.run()
